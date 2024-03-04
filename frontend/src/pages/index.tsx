e5f1162ed9983b24886a00bd0a360de8f29def5d
TimonAME <timon-a@outlook.at>
2024-03-04 08:31:10 +0100

    overflow-hidden TODO removed

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -15,7 +15,6 @@ export default function Index() {
             </div>
             <div className="flex justify-center">
                 <Untertext />
-                { /* TODO: Change Size to block scrolling */}
             </div>
             <div></div>
         </div>
