f88afd613583ba6e66c138ba5aa2e62f1f78f3c1
Jacob Schrott <0062@htl.rennweg.at>
2024-03-05 17:01:12 +0100

    Add Lobbyname component for prelobby
    Introduced a new Lobbyname component under the prelobby directory. This component renders the lobby name in a specific typographic style (i.e., 7xl, bold, and primary color), enhancing the visual emphasis of the component.

---
+++ frontend/src/components/prelobby/Lobbyname.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Lobbyname() {
+    return (
+        <div>
+            <h1 className={"text-7xl font-bold text-primary"}>Lobbyname</h1>
+        </div>
+    )
+}
+
+export default Lobbyname;
