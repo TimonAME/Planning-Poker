3e65eaea97ef671af2d16e54f75edc511963228a
Jacob Schrott <0062@htl.rennweg.at>
2024-03-07 21:49:45 +0100

    Refactor ThemeDropdown.vue to use v-for loop
    The code in ThemeDropdown.vue has been refactored to use a v-for loop instead of manually specifying each theme. This simplifies the code and improves maintainability. The themes are now stored in an array of objects, each with a label and value, to support this loop.

--- frontend/components/ThemeDropdown.vue
+++ frontend/components/ThemeDropdown.vue
@@ -18,179 +18,42 @@
             tabindex="0"
             class="dropdown-content bg-base-200 text-base-content rounded-box top-px h-[28.6rem] max-h-[calc(100vh-10rem)] w-56 overflow-y-auto border border-white/5 shadow-2xl outline outline-1 outline-black/5 mt-16"
         >
-            <li>
+            <li v-for="(theme, index) in themes" :key="index">
                 <input
                     type="radio"
                     name="theme-dropdown"
                     class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Light"
-                    value="light"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Dark"
-                    value="dark"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Retro"
-                    value="retro"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Cyberpunk"
-                    value="cyberpunk"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Valentine"
-                    value="valentine"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Aqua"
-                    value="aqua"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Emerald"
-                    value="emerald"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Synthwave"
-                    value="synthwave"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Pastel"
-                    value="pastel"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Wireframe"
-                    value="wireframe"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Black"
-                    value="black"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Luxury"
-                    value="luxury"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Dracula"
-                    value="dracula"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Business"
-                    value="business"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Lemonade"
-                    value="lemonade"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Night"
-                    value="night"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Winter"
-                    value="winter"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Coffee"
-                    value="coffee"
-                />
-            </li>
-            <li>
-                <input
-                    type="radio"
-                    name="theme-dropdown"
-                    class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                    aria-label="Sunset"
-                    value="sunset"
+                    :aria-label="theme.label"
+                    :value="theme.value"
+                    v-model="selectedTheme"
                 />
             </li>
         </ul>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+const themes = [
+    { label: "Light", value: "light" },
+    { label: "Dark", value: "dark" },
+    { label: "Retro", value: "retro" },
+    { label: "Cyberpunk", value: "cyberpunk" },
+    { label: "Valentine", value: "valentine" },
+    { label: "Aqua", value: "aqua" },
+    { label: "Emerald", value: "emerald" },
+    { label: "Synthwave", value: "synthwave" },
+    { label: "Pastel", value: "pastel" },
+    { label: "Wireframe", value: "wireframe" },
+    { label: "Black", value: "black" },
+    { label: "Luxury", value: "luxury" },
+    { label: "Dracula", value: "dracula" },
+    { label: "Business", value: "business" },
+    { label: "Lemonade", value: "lemonade" },
+    { label: "Night", value: "night" },
+    { label: "Winter", value: "winter" },
+    { label: "Coffee", value: "coffee" },
+    { label: "Sunset", value: "sunset" },
+];
+
+const selectedTheme = ref("light");
+</script>
