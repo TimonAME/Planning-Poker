da20120fde85d2acadc9ac63ed061ef6823e64d9
1FailX1 <1felixpayer1@gmail.com>
2024-03-12 17:03:29 +0100

    Added new directory for Backend

---
+++ backend/PlanningPoker/src/test/java/at/PlaPo/PlanningPoker/PlanningPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package at.PlaPo.PlanningPoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningPokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
