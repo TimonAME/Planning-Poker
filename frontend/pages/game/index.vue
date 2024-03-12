29131ea6c05e36478bc1c506d1c43261862f559d
TimonAME <timon-a@outlook.at>
2024-03-12 15:56:27 +0100

    game index simple setup

--- frontend/pages/game/index.vue
+++ frontend/pages/game/index.vue
@@ -1,9 +1,22 @@
 <template>
-    <div class="flex flex-col justify-center items-center h-screen">
-        <router-link to="/" class="link link-primary">Home</router-link>
+    <div class="absolute inset-0 -z-10 h-full w-full">
+        <!-- dotted background from: https://bg.ibelick.com/ -->
+        <!-- Punkte im Hintergrund: bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px] -->
+        <div
+            class="flex flex-col justify-start h-screen overflow-hidden sm:mx-0 mx-4"
+        >
+            <Navbar class="z-20" />
+            <DropdownMenu class="z-20" />
+            <div class="flex flex-col justify-center items-center h-screen">
+                <router-link to="/" class="link link-primary">Home</router-link>
+            </div>
+        </div>
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+import DropdownMenu from "~/components/prelobby/Lobbyname.vue";
+import Navbar from "~/components/prelobby/Navbar.vue";
+</script>
 
 <style lang="scss" scoped></style>
