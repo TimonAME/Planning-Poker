b620884ea186f4597d987fd92173f16d113dc747
1FailX1 <1felixpayer1@gmail.com>
2024-03-12 17:37:52 +0100

    mid dev

---
+++ backend/PlanningPoker/src/main/java/at/PlaPo/PlanningPoker/Lobby.java
@@ -0,0 +1,15 @@
+package at.PlaPo.PlanningPoker;
+
+public class Lobby {
+    private String key;
+    public Lobby(String key){
+        this.key = key;
+    }
+    public String getKey(){
+        return this.key;
+    }
+    public void setKey(String key){
+        this.key = key;
+    }
+
+}
