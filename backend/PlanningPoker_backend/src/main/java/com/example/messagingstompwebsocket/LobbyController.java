d756e0c9c1604167cf7c2eef8d90b6033b67d460
Hunor Zakarias <hunor.zakarias@gmail.com>
2024-04-09 17:43:12 +0200

    cleaned folder structure

---
+++ backend/PlanningPoker_backend/src/main/java/com/example/messagingstompwebsocket/LobbyController.java
@@ -0,0 +1,40 @@
+package com.example.messagingstompwebsocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import com.example.messagingstompwebsocket.lobby.Lobby;
+import com.example.messagingstompwebsocket.lobby.LobbyJoinRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Controller
+public class LobbyController {
+
+    HashMap<String, User> users;
+    ArrayList<Lobby> allLobbies = new ArrayList<>();
+    HashMap<String, Lobby> lobbies = new HashMap<>();
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @MessageMapping("/join")
+    public void joinLobby(LobbyJoinRequest lobbyJoinRequest) {
+        System.out.println("User joining: " + lobbyJoinRequest.getUserHash());
+        lobbies.get(lobbyJoinRequest.getLobbyHash()).userHashes.add(lobbyJoinRequest.getUserHash());
+    }
+
+    @MessageMapping("/create")
+    public void createLobby(Lobby lobby) {
+        System.out.println(lobby.lobbyHash);
+        lobbies.put(lobby.getLobbyHash(), lobby);
+    }
+
+    @MessageMapping("/lobby/message/{id}")
+    public void communicate(@DestinationVariable String id, Message message) throws Exception {
+        System.out.println(message);
+        simpMessagingTemplate.convertAndSend("/topic/lobby/" + id, message);
+    }
+}
