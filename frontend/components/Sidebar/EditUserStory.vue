981a5b7ed58d83b1fa7da8c08b9b66b500c69218
TimonAME <timon-a@outlook.at>
2024-05-03 09:28:58 +0200

    added enter support for username-input.vue & EditUserStory.vue

--- frontend/components/Sidebar/EditUserStory.vue
+++ frontend/components/Sidebar/EditUserStory.vue
@@ -33,19 +33,20 @@
                     ✕
                 </button>
             </form>
-            <h3 class="font-bold text-lg">Edit User Story</h3>
-            <label class="form-control w-full">
-                <div class="label">
-                    <span class="label-text">Title</span>
-                </div>
-                <div :class="{ error: v$.userStoryTitle.$errors.length }">
-                    <input
-                        type="text"
-                        placeholder="Type here"
-                        class="input input-bordered w-full"
-                        v-model="userStoryTitle"
-                    />
-                    <!--
+            <form @submit.prevent="editUserStory">
+                <h3 class="font-bold text-lg">Edit User Story</h3>
+                <label class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text">Title</span>
+                    </div>
+                    <div :class="{ error: v$.userStoryTitle.$errors.length }">
+                        <input
+                            type="text"
+                            placeholder="Type here"
+                            class="input input-bordered w-full"
+                            v-model="userStoryTitle"
+                        />
+                        <!--
                     <div
                         class="input-errors"
                         v-for="error of v$.userStoryTitle.$errors"
@@ -55,24 +56,28 @@
                             {{ error.$message }}
                         </div>
                     </div>
-                    -->
+                    --></div>
+                </label>
+                <label class="form-control">
+                    <div class="label">
+                        <span class="label-text">Description</span>
+                    </div>
+                    <textarea
+                        class="textarea textarea-bordered h-24"
+                        placeholder="Type here"
+                        v-model="userStoryDescription"
+                    ></textarea>
+                </label>
+                <div class="flex justify-end">
+                    <button
+                        type="submit"
+                        class="btn btn-primary mt-3"
+                        @click="editUserStory"
+                    >
+                        Edit
+                    </button>
                 </div>
-            </label>
-            <label class="form-control">
-                <div class="label">
-                    <span class="label-text">Description</span>
-                </div>
-                <textarea
-                    class="textarea textarea-bordered h-24"
-                    placeholder="Type here"
-                    v-model="userStoryDescription"
-                ></textarea>
-            </label>
-            <div class="flex justify-end">
-                <button class="btn btn-primary mt-3" @click="editUserStory">
-                    Edit
-                </button>
-            </div>
+            </form>
         </div>
         <form method="dialog" class="modal-backdrop">
             <button>close</button>
--- frontend/components/Sidebar/ManualUserStory.vue
+++ frontend/components/Sidebar/ManualUserStory.vue
@@ -11,19 +11,20 @@
                     ✕
                 </button>
             </form>
-            <h3 class="font-bold text-lg">Add new User Story</h3>
-            <label class="form-control w-full">
-                <div class="label">
-                    <span class="label-text">Title</span>
-                </div>
-                <div :class="{ error: v$.userStoryTitle.$errors.length }">
-                    <input
-                        type="text"
-                        placeholder="Type here"
-                        class="input input-bordered w-full"
-                        v-model="userStoryTitle"
-                    />
-                    <!--
+            <form @submit.prevent="addTheUserStory">
+                <h3 class="font-bold text-lg">Add new User Story</h3>
+                <label class="form-control w-full">
+                    <div class="label">
+                        <span class="label-text">Title</span>
+                    </div>
+                    <div :class="{ error: v$.userStoryTitle.$errors.length }">
+                        <input
+                            type="text"
+                            placeholder="Type here"
+                            class="input input-bordered w-full"
+                            v-model="userStoryTitle"
+                        />
+                        <!--
                     <div
                         class="input-errors"
                         v-for="error of v$.userStoryTitle.$errors"
@@ -33,24 +34,28 @@
                             {{ error.$message }}
                         </div>
                     </div>
-                    -->
+                    --></div>
+                </label>
+                <label class="form-control">
+                    <div class="label">
+                        <span class="label-text">Description</span>
+                    </div>
+                    <textarea
+                        class="textarea textarea-bordered h-24"
+                        placeholder="Type here"
+                        v-model="userStoryDescription"
+                    ></textarea>
+                </label>
+                <div class="flex justify-end">
+                    <button
+                        type="submit"
+                        class="btn btn-primary mt-3"
+                        @click="addTheUserStory"
+                    >
+                        Add
+                    </button>
                 </div>
-            </label>
-            <label class="form-control">
-                <div class="label">
-                    <span class="label-text">Description</span>
-                </div>
-                <textarea
-                    class="textarea textarea-bordered h-24"
-                    placeholder="Type here"
-                    v-model="userStoryDescription"
-                ></textarea>
-            </label>
-            <div class="flex justify-end">
-                <button class="btn btn-primary mt-3" @click="addTheUserStory">
-                    Add
-                </button>
-            </div>
+            </form>
         </div>
         <form method="dialog" class="modal-backdrop">
             <button>close</button>
