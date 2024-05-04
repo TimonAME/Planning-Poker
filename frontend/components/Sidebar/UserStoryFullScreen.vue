33fc75408118ce1ae619f94e177256a067b5a774
Jacob Schrott <0062@htl.rennweg.at>
2024-05-04 17:09:42 +0200

    Implement styled descriptions in UserStoryFullScreen component
    The update allows HTML content in the UserStory description to be styled dynamically. This change includes the application of styling on 'h1', 'ul', and 'li' tags in the description to enhance readability and provide better user experience.

--- frontend/components/Sidebar/UserStoryFullScreen.vue
+++ frontend/components/Sidebar/UserStoryFullScreen.vue
@@ -10,7 +10,7 @@
                 </button>
             </form>
             <h3 class="font-bold text-lg">{{ props.userStory.title }}</h3>
-            <p class="py-4">{{ props.userStory.description }}</p>
+            <p class="py-4" v-html="styledDescription"></p>
         </div>
         <form method="dialog" class="modal-backdrop">
             <button>close</button>
@@ -27,6 +27,24 @@ const props = defineProps({
         required: true,
     },
     index: Number,
+});
+
+const styledDescription = computed(() => {
+    let htmlContent = props.userStory.description;
+
+    // Applying styles to <h1> tags
+    htmlContent = htmlContent.replace(
+        /<h1>/g,
+        '<h1 class="text-xl font-bold text-primary mt-2 mb-4">',
+    );
+
+    // Applying styles to <ul> tags
+    htmlContent = htmlContent.replace(/<ul>/g, '<ul class="list-disc pl-5">');
+
+    // Applying styles to <li> tags
+    htmlContent = htmlContent.replace(/<li>/g, '<li class="mb-1">');
+
+    return htmlContent;
 });
 
 const modalId = ref(`my_modal_${props.index + 10000}`);
