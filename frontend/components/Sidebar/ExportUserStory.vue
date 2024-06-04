5e15800a09c8e1420a2cf9abf70ce73e955ef0d7
TimonAME <timon-a@outlook.at>
2024-06-04 17:41:43 +0200

    Export einstellungen

--- frontend/components/Sidebar/ExportUserStory.vue
+++ frontend/components/Sidebar/ExportUserStory.vue
@@ -1,12 +1,67 @@
 <template>
+    <!--
     <button class="btn btn-outline btn-error" @click="exportFunction">
         Export
     </button>
+-->
+    <div class="dropdown dropdown-end z-50">
+        <button
+            class="btn btn-outline btn-error"
+            @click="toggleDropdown(false)"
+            tabindex="0"
+            role="button"
+        >
+            Export
+        </button>
+        <div
+            tabindex="0"
+            class="dropdown-content z-[1] card card-compact w-64 p-2 shadow bg-base-100 text-primary-content"
+            v-show="dropdownOpen"
+        >
+            <div class="card-body">
+                <h3 class="card-title text-error">
+                    Select what you want in your export!
+                </h3>
+                <!--<form @submit.prevent="">-->
+                <div class="form-control">
+                    <label class="label cursor-pointer">
+                        <span class="label-text">User Stories</span>
+                        <input
+                            type="checkbox"
+                            checked="checked"
+                            class="checkbox"
+                            v-model="exportUserStories"
+                        />
+                    </label>
+                    <label class="label cursor-pointer">
+                        <span class="label-text">User-List</span>
+                        <input
+                            type="checkbox"
+                            class="checkbox"
+                            v-model="exportUserList"
+                        />
+                    </label>
+                </div>
+                <button class="btn btn-neutral" @click="exportFunction">
+                    Export
+                </button>
+                <!--</form>-->
+            </div>
+        </div>
+    </div>
 </template>
 <script setup>
 import { ref } from "vue";
 import { useUserStoryStore } from "@/stores/userStory";
 import { useLobbyStore } from "~/stores/lobby.js";
+import { useUserStore } from "~/stores/user.js";
+
+const userStoryStore = useUserStoryStore();
+const userStories = userStoryStore.userStories;
+const userStore = useUserStore();
+
+const exportUserStories = ref(true);
+const exportUserList = ref(false);
 
 const removeHtmlTags = (str) => {
     if (typeof str !== "string") {
@@ -16,37 +71,55 @@ const removeHtmlTags = (str) => {
 };
 
 const exportFunction = () => {
-    const userStoryStore = useUserStoryStore();
-    const userStories = userStoryStore.userStories;
-
     if (userStories.length === 0) {
         console.log("No user stories to export");
         return;
     }
 
     let csvContent = "";
-    userStories.forEach((userStory, index) => {
-        // Exclude 'voted' attribute and change 'size' to 'not voted' if null
-        let filteredUserStory = {};
-        for (let key in userStory) {
-            if (key !== "voted" && key !== "originalIndex") {
-                if (key === "size" && userStory[key] === null) {
-                    filteredUserStory[key] = "not voted";
-                } else {
-                    filteredUserStory[key] = userStory[key];
+    // Export User Stories
+    if (exportUserStories.value && userStories.length > 0) {
+        userStories.forEach((userStory, index) => {
+            // Exclude 'voted' attribute and change 'size' to 'not voted' if null
+            let filteredUserStory = {};
+            for (let key in userStory) {
+                if (key !== "voted" && key !== "originalIndex") {
+                    if (key === "size" && userStory[key] === null) {
+                        filteredUserStory[key] = "not voted";
+                    } else {
+                        filteredUserStory[key] = userStory[key];
+                    }
                 }
             }
-        }
 
-        if (index === 0) {
-            // Add header row
-            csvContent += "Title;Description;StoryPoints\n";
+            if (index === 0) {
+                // Add header row
+                csvContent += "Title;Description;StoryPoints\n";
+            }
+            let row = Object.values(filteredUserStory)
+                .map((value) => `"${removeHtmlTags(value)}"`)
+                .join(";");
+            csvContent += row + "\n";
+        });
+    }
+
+    // Export User List
+    if (exportUserList.value) {
+        if (exportUserStories.value) {
+            csvContent += "\n";
         }
-        let row = Object.values(filteredUserStory)
-            .map((value) => `"${removeHtmlTags(value)}"`)
-            .join(";");
-        csvContent += row + "\n";
-    });
+        const userList = userStore.userList;
+        if (userList.length > 0) {
+            userList.forEach((user, index) => {
+                if (index === 0) {
+                    // Add header row
+                    csvContent += "Username\n";
+                }
+                let row = `"${user.name}"`;
+                csvContent += row + "\n";
+            });
+        }
+    }
 
     const blob = new Blob([csvContent], { type: "text/csv;charset=utf-8;" });
     const url = URL.createObjectURL(blob);
@@ -66,5 +139,16 @@ const exportFunction = () => {
     );
     document.body.appendChild(link);
     link.click();
+
+    toggleDropdown(true);
+};
+
+const dropdownOpen = ref(true);
+const toggleDropdown = (overwrite) => {
+    if (overwrite) {
+        dropdownOpen.value = !dropdownOpen.value;
+    } else {
+        dropdownOpen.value = true;
+    }
 };
 </script>
