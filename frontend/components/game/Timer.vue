5617d98dd4629bd428d804550e0456cae34c3ae4
TimonAME <timon-a@outlook.at>
2024-06-06 16:37:24 +0200

    :/

--- frontend/components/game/Timer.vue
+++ frontend/components/game/Timer.vue
@@ -51,17 +51,41 @@ const startCountdown = () => {
         }
     }, 1000);
 };
+
+let paused = ref(false);
+const pauseTimer = () => {
+    //pause or unpause timer
+    if (interval) {
+        clearInterval(interval);
+        interval = null;
+        paused.value = true;
+    } else {
+        startCountdown();
+        paused.value = false;
+    }
+};
 </script>
 
 <template>
-    <div class="fixed top-20 left-8" v-if="useCountdown">
-        <span
-            class="countdown font-mono text-6xl transition-colors duration-500 ease-in-out"
-            :class="{ 'text-error': overtime }"
-        >
-            <span :style="`--value:${countdownMin}`"></span>:
-            <span :style="`--value:${countdownSec}`"></span>
-        </span>
+    <div
+        class="fixed top-20 left-8 cursor-pointer"
+        v-if="useCountdown"
+        @click="pauseTimer"
+    >
+        <div class="indicator">
+            <span
+                class="indicator-item badge badge-primary transition-opacity"
+                :class="{ 'opacity-0': !paused }"
+                >paused</span
+            >
+            <span
+                class="countdown font-mono text-6xl transition-colors duration-500 ease-in-out"
+                :class="{ 'text-error': overtime }"
+            >
+                <span :style="`--value:${countdownMin}`"></span>:
+                <span :style="`--value:${countdownSec}`"></span>
+            </span>
+        </div>
     </div>
 </template>
 
