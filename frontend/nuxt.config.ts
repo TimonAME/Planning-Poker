ad33618bce1d1d61b29b151f6e3713722a38a2b5
Timon <timon-a@outlook.at>
2024-12-14 13:58:42 +0100

    nuxt update 12

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -12,6 +12,5 @@ export default defineNuxtConfig({
     ssr: false,
     app: {
         baseURL: '/Planning-Poker/', // baseURL: '/<repository>/'
-        buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
     }
 });
