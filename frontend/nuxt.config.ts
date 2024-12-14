50ce684188f3471637439de7a0cc29e29baa63c6
Timon <timon-a@outlook.at>
2024-12-14 13:49:12 +0100

    nuxt update 10

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -10,4 +10,8 @@ export default defineNuxtConfig({
     primevue: {},
     css: ["primevue/resources/themes/aura-light-green/theme.css", "@/assets/css/global.css"],
     ssr: false,
+    app: {
+        baseURL: '/Planning-Poker/', // baseURL: '/<repository>/'
+        buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+    }
 });
